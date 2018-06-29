6a5dbdb7cc37430dddbaa3e9ed9ddc95dcd11b31
江德炜 <jiangdewei@innotechx.com>
2018-06-29 10:18:51 +0800

    init

---
+++ .next/4.081a177ace33dcd6d74c.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate(4,{
+
+/***/ "./node_modules/webpack/buildin/harmony-module.js":
+false,
+
+/***/ "./pages/index.js":
+/***/ (function(module, __webpack_exports__) {
+
+"use strict";
+throw new Error("Module build failed: SyntaxError: /Users/dw/Desktop/DW/next_learn/pages/index.js: Unterminated JSX contents (6:8)\n\n  4 |   <div>\n  5 |     <p>Hello Next.js\n> 6 |   </div>\n    |         ^\n  7 | )\n  8 | \n  9 | export default Index;\n    at _class.raise (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:778:15)\n    at _class.jsxReadToken (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:7727:16)\n    at _class.readToken (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:8095:21)\n    at _class.nextToken (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:976:12)\n    at _class.next (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:912:10)\n    at _class.eat (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:917:12)\n    at _class.expect (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:2051:10)\n    at _class.jsxParseClosingElementAt (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:8001:12)\n    at _class.jsxParseElementAt (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:8020:39)\n    at _class.jsxParseElementAt (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:8024:34)\n    at _class.jsxParseElement (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:8077:19)\n    at _class.parseExprAtom (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:8084:21)\n    at _class.parseExprSubscripts (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:2725:21)\n    at _class.parseMaybeUnary (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:2704:21)\n    at _class.parseExprOps (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:2616:21)\n    at _class.parseMaybeConditional (/Users/dw/Desktop/DW/next_learn/node_modules/babylon/lib/index.js:2588:21)");
+
+/***/ })
+
+})
+//# sourceMappingURL=4.081a177ace33dcd6d74c.hot-update.js.map
